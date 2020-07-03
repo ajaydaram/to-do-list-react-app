1a9993746099bea2c68474215cbbb61936705ce2
Daram Ajay <ajayforchrist777@gmail.com>
2020-07-03 08:18:48 +0000

    created to-do-item component

---
+++ todo-list/src/toDoItem.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const toDoItem = () => {
+    return (
+        <div>
+            <input type="text" />
+            <p>Here goes the text</p>
+        </div>
+    )
+}
+
+export default toDoItem
